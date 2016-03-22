34d72877b93e932c5c3417579fa5da7676af20f2
KevinBozic <bozic.kevin@gmail.com>
2016-03-22 12:13:41 -0400

    Parse csv file

--- src/people_closure/core.clj
+++ src/people_closure/core.clj
@@ -1,5 +1,23 @@
 (ns people-closure.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk])
   (:gen-class))
 
 (defn -main []
-  (println "Hello, World!"))
+  (let [people (slurp "people.csv")
+        people (str/split-lines people)
+        people (map (fn [line]
+                      (str/split line #","))
+                    people)
+        header (first people)
+        people (rest people)
+        people (map (fn [line]
+                      (apply hash-map (interleave header line)))
+                 people)
+        people (walk/keywordize-keys people)
+        people (filter (fn [line]
+                         (= (:country line) "Brazil"))
+                       people)]  
+    (spit "filtered_people.edn" (pr-str people))
+    people))
+  
