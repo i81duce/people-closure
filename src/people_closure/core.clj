30cee8efa7e80bec00d23b3e214ef522959cfb1d
KevinBozic <bozic.kevin@gmail.com>
2016-03-23 10:47:33 -0400

    Add routes

--- src/people_closure/core.clj
+++ src/people_closure/core.clj
@@ -1,9 +1,11 @@
 (ns people-closure.core
   (:require [clojure.string :as str]
-            [clojure.walk :as walk])
+            [clojure.walk :as walk]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j])
   (:gen-class))
 
-(defn -main []
+(defn read-people []
   (let [people (slurp "people.csv")
         people (str/split-lines people)
         people (map (fn [line]
@@ -18,6 +20,12 @@
         people (filter (fn [line]
                          (= (:country line) "Brazil"))
                        people)]  
-    (spit "filtered_people.edn" (pr-str people))
+    ;(spit "filtered_people.edn" (pr-str people))
     people))
-  
+
+(c/defroutes app
+  (c/GET "/" request
+    "Hello, world!"))
+
+(defn -main []
+  (j/run-jetty app {:port 3000}))
